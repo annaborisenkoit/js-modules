cd4268b56bde6f0a53f054d6eefd4d3d4f7b323d
Anna Borisenko <anik4366@mail.ru>
2023-11-21 11:02:23 +0700

    export default

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import {
     sum,
     PI
 } from './math.js';
+import Model from './model.js';
 
 const aInputNode = document.querySelector('#a');
 const bInputNode = document.querySelector('#b');
 const resBtnNode = document.querySelector('#result-btn');
 const outputNode = document.querySelector('#output');
+
+const model = new Model()
 
 resBtnNode.addEventListener('click', () => {
     const a = parseInt(aInputNode.value);
@@ -16,4 +19,5 @@ resBtnNode.addEventListener('click', () => {
     outputNode.innerText = multiply(a, b);
 
     console.log(PI);
+    console.log(model.getTodos());
 })
